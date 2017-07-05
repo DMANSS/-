3def0c8f4021cfc88c31e093139c5c9aeffe704e
User <User@DEADUSER>
2017-07-05 23:46:50 +0300

    Добавьте файлы проекта.

---
+++ LibraryAppSmirnov/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryAppSmirnov.Startup))]
+namespace LibraryAppSmirnov
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
